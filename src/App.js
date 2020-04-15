8d7f52a7230112186411c4b5b79788f58478b737
Jon-Stickrod <jonstickrodCodes@gmail.com>
2020-04-14 20:14:16 -0700

    data imported

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+
+export const dataList = () => {
+
+};
+
+
+
+
 
 function App() {
+  const [data, setData] = useState({})
+useEffect(() =>{
+axios
+  .get('https://api.nasa.gov/planetary/apod?api_key=0aX8vfY6iYMaUVvPBepSbgJlHW8tV6cQ8KW8jeQT')
+  .then(res =>{
+    console.log(res)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+},[])
+
+
+  
   return (
     <div className="App">
       <p>
@@ -13,3 +36,4 @@ function App() {
 }
 
 export default App;
+
